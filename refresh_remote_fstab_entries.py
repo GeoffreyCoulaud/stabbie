dbed3154f7539465dedb764cf936ecc5e0fb8321
GeoffreyCoulaud <geoffrey.coulaud@gmail.com>
2023-08-03 13:42:37 +0200

    Support for custom nfs port

--- refresh_remote_fstab_entries.py
+++ refresh_remote_fstab_entries.py
@@ -112,7 +112,7 @@ class FstabEntry:
     name: str
     mount_point: MountPoint
     fs_type: str
-    mount_options: list[str]
+    mount_options: Sequence[str]
     dump_frequency: int = 0
     fsck_pass_number: int = 0
 
@@ -158,12 +158,45 @@ class RemoteFstabEntry(FstabEntry):
 
 
 @dataclass
+class NfsMountOptions:
+    version: int
+    port: int
+
+    @classmethod
+    def from_options_sequence(cls, options: Sequence[str]) -> "NfsMountOptions":
+        """Create nfs mount options from a raw options string"""
+
+        # Get the options that are in key=value format
+        options_mapping = {}
+        for option in options:
+            try:
+                key, value = option.split("=", maxsplit=1)
+            except ValueError:
+                continue
+            options_mapping[key] = value
+
+        # Get nfs specific options
+        version = int(options_mapping.get("version", 3))
+        default_port = 2049 if version == 4 else 0
+        port = int(options_mapping.get("port", default_port))
+        return NfsMountOptions(version, port)
+
+
+@dataclass
 class NfsFstabEntry(RemoteFstabEntry):
     remote_path: str = field(init=False)
 
     def __post_init__(self) -> None:
-        host, remote_path = self.name.split(":")
-        self.service = Service.new_cached(host, 2049)
+        """Generate the values specific to NFS entries from its values"""
+
+        # Get host and remote path
+        # Split once from the right to allow raw IPv6 hosts
+        host, remote_path = self.name.rsplit(":", maxsplit=1)
+
+        # Get the NFS specific mount options
+        options = NfsMountOptions.from_options_sequence(self.mount_options)
+
+        self.service = Service.new_cached(host, options.port)
         self.remote_path = remote_path
 
 
@@ -182,7 +215,7 @@ class FstabEntryBuilder:
 
         # Get the different items
         name, mount_point_path, fs_type, *optionals = segments
-        mount_options = optionals[0].split(",") if len(optionals) > 0 else []
+        mount_options = optionals[0].split(",") if len(optionals) > 0 else ""
         dump_frequency = int(optionals[1]) if len(optionals) > 1 else 0
         fsck_pass_number = int(optionals[2]) if len(optionals) > 2 else 0
         mount_point = MountPoint(mount_point_path)
